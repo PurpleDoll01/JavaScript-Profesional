b1f9a12376381cbf730139d4afed3218d882764c
Mariana Valencia <purple.mv@gmail.com>
2019-10-09 15:58:15 -0500

    Basic Types

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,7 +1,39 @@
-console.log("Hello, TypeScript");
+//Boolean 
+let muted: boolean = true;
+muted = false; 
 
-function add(a: number, b: number) {
-    return a + b;
+
+//Números
+let numerador: number = 42;
+let denominador: number = 6;
+let resultado = numerador / denominador;
+
+//String
+let nombre: string = 'Richard';
+let saludo = `Me llamo ${nombre}`;
+
+//Arreglos
+let people: string[] = [];
+people = ["Isabel", "Nicole", "Raul"];
+people.push("900");
+
+let peopleAndNumbers: Array< string | number > = [];
+peopleAndNumbers.push("Ricardo");
+peopleAndNumbers.push(90);
+
+//Enum
+enum Color {
+    Rojo = "Rojo",
+    Verde = "Verde",
+    Azul = "Azul",
 }
 
-const sum = add(2,3);
+let colorFavorito: Color = Color.Azul;
+console.log(`Mi color favorito es ${colorFavorito}`);
+
+//Any
+let comodin: any = "Joker";
+comodin = { type: "Wildcard" }
+
+//Object
+let someObject: object = { type: "Wildcard" }
