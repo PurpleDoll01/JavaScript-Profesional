eef8d68ab1826dad0e286866c3ca01af01b08a0d
Mariana Valencia <purple.mv@gmail.com>
2019-10-09 18:11:54 -0500

    Proyect to TypeScript

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,11 +1,10 @@
-function MediaPlayer(config) {
+class MediaPlayer {
+  constructor(config) {
     this.media = config.el;
     this.plugins = config.plugins || [];
-
     this._initPlugins();
   }
-
-  MediaPlayer.prototype._initPlugins = function() {
+  _initPlugins() {
     const player = {
       play: () => this.play(),
       pause: () => this.pause(),
@@ -13,39 +12,40 @@ function MediaPlayer(config) {
       get muted() {
         return this.media.muted;
       },
-
       set muted(value) {
         this.media.muted = value;
       }
     };
-
     this.plugins.forEach(plugin => {
-       plugin.run(player); 
-    })  
+      plugin.run(player);
+    });
   }
-
-  MediaPlayer.prototype.play = function() {
+  play() {
     this.media.play();
-  };
-
-  MediaPlayer.prototype.pause = function() {
+  }
+  pause() {
     this.media.pause();
-  };
-
-  MediaPlayer.prototype.togglePlay = function() {
+  }
+  togglePlay() {
     if (this.media.paused) {
       this.play();
-    } else {
+    }
+    else {
       this.pause();
     }
-  };
-
-  MediaPlayer.prototype.mute = function() {
+  }
+  mute() {
     this.media.muted = true;
-  };
-
-  MediaPlayer.prototype.unmute = function() {
+  }
+  unmute() {
     this.media.muted = false;
-  };
+  }
+}
+
+
+
+
+
+
 
   export default MediaPlayer;
